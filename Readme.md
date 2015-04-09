e8f1cd796f8ffee1c6776d79d7f2341bcee38db9
Joseph Lavin <josephlavin@gmail.com>
2015-04-08 21:51:39 -0700

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Slate Configuration File
+
+This is a `.slate.js` file for the [Slate Window Management System](https://github.com/mattr-/slate).
+
+> Slate is a window management application similar to Divvy and SizeUp (except better and free!). Originally written to replace them due to some limitations in how each work, it attempts to overcome them by simply being extremely configurable. As a result, it may be a bit daunting to get configured, but once it is done, the benefit is huge.
+
+The layout is based on a windows only application "WinSplit Revolution".  For a quick overview [watch this youtube video](https://www.youtube.com/watch?v=o1AtHyf4pRQ).
+
+I switch from a desktop full keyboard to a laptop so this includes two sets of keyboard bindings. 
+
+Keyboard shortcuts for a full keyboard can be access via. `cmd`+`number pad #`.
+
+Keyboard shortcuts for a smaller keyboard (without number pad) can be access via. `cmd`+`option`+`arrow key` and `cmd`+`option`+`shift`+`arrow key`.
+
+## Install
+See [mattr-/slate](https://github.com/mattr-/slate#installing-slate) for install instructions.  Once installed put [slate.js](slate.js) into your `home` directory as `.slate.js`.
---
+++ slate.js
@@ -0,0 +1,191 @@
+/**
+ * Right/Left Edge Position Placement
+ * Cycle through 3 different sizes
+ *
+ * @param win window object
+ * @param lastPos current tracked position
+ * @param direction right|left
+ * @returns {number} position to track
+ */
+var pushToVerticalEdge = function(win, lastPos, direction){
+    switch (lastPos) {
+        case 2:
+            win.doOperation(slate.operation("push", {
+                "direction" : direction,
+                "style" : "bar-resize:screenSizeX*.66"
+            }));
+            return 3;
+            break;
+
+        case 1:
+            win.doOperation(slate.operation("push", {
+                "direction" : direction,
+                "style" : "bar-resize:screenSizeX*.34"
+            }));
+            return 2;
+            break;
+
+        default:
+            win.doOperation(slate.operation("push", {
+                "direction" : direction,
+                "style" : "bar-resize:screenSizeX*.5"
+            }));
+            return 1;
+            break;
+    }
+};
+
+/**
+ * Top & Bottom Position Placement
+ * Cycle through 2 sizes
+ *
+ * @param win window object
+ * @param lastPos current tracked position
+ * @param direction top|bottom
+ * @returns {number}  position to track
+ */
+var pushToHorizontalEdge = function(win, lastPos, direction){
+    switch (lastPos) {
+        case 1:
+            var x = "screenSizeX*.34";
+            var y = "0";
+            if(direction == 'bottom') {
+                y = "screenOriginY+(screenSizeY/2)";
+            }
+            win.doOperation(slate.operation("move", {
+                "x": x,
+                "y": y,
+                "width" : "screenSizeX*.32",
+                "height": "screenSizeY*.5"
+            }));
+            return 2;
+            break;
+
+        default:
+            win.doOperation(slate.operation("push", {
+                "direction" : direction,
+                "style" : "bar-resize:screenSizeY*.5"
+            }));
+            return 1;
+            break;
+    }
+};
+
+var leftPos = 0;
+slate.bind("pad4:cmd", function(win) {
+    leftPos = pushToVerticalEdge(win, leftPos, 'left');
+});
+slate.bind("left:cmd,alt", function(win) {
+    leftPos = pushToVerticalEdge(win, leftPos, 'left');
+});
+
+var rightPos = 0;
+slate.bind("pad6:cmd", function(win) {
+    rightPos = pushToVerticalEdge(win, rightPos, 'right');
+});
+slate.bind("right:cmd,alt", function(win) {
+    rightPos = pushToVerticalEdge(win, rightPos, 'right');
+});
+
+var topPos = 0;
+slate.bind("pad8:cmd", function(win) {
+    topPos = pushToHorizontalEdge(win, topPos, 'top');
+});
+slate.bind("up:cmd,alt", function(win) {
+    topPos = pushToHorizontalEdge(win, topPos, 'top');
+});
+
+var bottomPos = 0;
+slate.bind("pad2:cmd", function(win) {
+    bottomPos = pushToHorizontalEdge(win, bottomPos, 'bottom');
+});
+slate.bind("down:cmd,alt", function(win) {
+    bottomPos = pushToHorizontalEdge(win, bottomPos, 'bottom');
+});
+
+/**
+ * Push to Corner
+ * Cycle through 3 sizes
+ *
+ * @param win window object
+ * @param lastPos current tracked position
+ * @param direction top-left|top-right|bottom-left|bottom-right
+ * @returns {number} position to track
+ */
+var pushToCorner = function(win, lastPos, direction){
+    var width = '';
+    switch (lastPos) {
+        case 2:
+            width = "screenSizeX*.66";
+            lastPos = 3;
+            break;
+
+        case 1:
+            width = "screenSizeX*.34";
+            lastPos = 2;
+            break;
+
+        default:
+            width = "screenSizeX*.5";
+            lastPos = 1;
+            break;
+    }
+
+    win.doOperation(slate.operation("corner", {
+        "direction" : direction,
+        "width" : width,
+        "height" : "screenSizeY*.5"
+    }));
+
+    return lastPos;
+};
+
+var cornerTopLeft = 0;
+slate.bind("pad7:cmd", function(win) {
+    cornerTopLeft = pushToCorner(win, cornerTopLeft, 'top-left');
+});
+slate.bind("left:cmd,alt,shift", function(win) {
+    cornerTopLeft = pushToCorner(win, cornerTopLeft, 'top-left');
+});
+
+var cornerTopRight = 0;
+slate.bind("pad9:cmd", function(win) {
+    cornerTopRight = pushToCorner(win, cornerTopRight, 'top-right');
+});
+slate.bind("right:cmd,alt,shift", function(win) {
+    cornerTopRight = pushToCorner(win, cornerTopRight, 'top-right');
+});
+
+var cornerBottomLeft = 0;
+slate.bind("pad1:cmd", function(win) {
+    cornerBottomLeft = pushToCorner(win, cornerBottomLeft, 'bottom-left');
+});
+slate.bind("up:cmd,alt,shift", function(win) {
+    cornerBottomLeft = pushToCorner(win, cornerBottomLeft, 'bottom-left');
+});
+
+var cornerBottomRight = 0;
+slate.bind("pad3:cmd", function(win) {
+    cornerBottomRight = pushToCorner(win, cornerBottomRight, 'bottom-right');
+});
+slate.bind("down:cmd,alt,shift", function(win) {
+    cornerBottomRight = pushToCorner(win, cornerBottomRight, 'bottom-right');
+});
+
+/**
+ * Full Screen
+ */
+var fullScreen = function(win){
+    win.doOperation(slate.operation("corner", {
+        "direction" : 'top-left',
+        "width" : "screenSizeX",
+        "height" : "screenSizeY"
+    }));
+};
+
+slate.bind("pad5:cmd", function(win) {
+    fullScreen(win);
+});
+slate.bind("f:cmd,alt", function(win) {
+    fullScreen(win);
+});
